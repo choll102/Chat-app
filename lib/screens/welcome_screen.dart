85080df0ad0f8fd733a2df940aca21f392644e4e
choll102 <17dh110778@st.huflit.edu.vn>
2020-10-13 23:45:14 +0700

    first commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,88 @@
+import 'package:chatapp/screens/login_screen.dart';
+import 'package:chatapp/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:chatapp/components/rounded_button.dart';
+class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: Container(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Image.asset('images/logo.png'),
+                      height: 60.0,
+                    ),
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(title: 'Đăng Nhập',colour: Colors.lightBlueAccent,onPressed: (){
+              Navigator.pushNamed(context, LoginScreen.id);
+            },),
+            RoundedButton(title: 'Đăng Kí',colour: Colors.blueAccent,onPressed: (){
+              Navigator.pushNamed(context, RegistrationScreen.id);
+            },),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
